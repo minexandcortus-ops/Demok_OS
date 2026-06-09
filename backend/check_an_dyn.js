1314bb7c017775c34444b0a102cdfdb5228c871f
Cortus <minexandcortus@gmail.com>
2026-06-09 17:40:36 +0200

    Nettoyage des scripts de tests et fichiers temporaires

--- backend/check_an_dyn.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-https.get('https://www.assemblee-nationale.fr/dyn/17/scrutins', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => {
-    const matches = data.match(/\/dyn\/17\/scrutins\/\d+/g);
-    console.log("Derniers scrutins AN (via /dyn/17/scrutins):", matches ? matches.slice(0, 5) : 'aucun');
-  });
-}).on('error', err => console.log(err));
--- backend/check_users.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg'); 
-const client = new Client({ user: 'demok_user', password: 'demok_password', database: 'demok_db', host: 'localhost', port: 5432 }); 
-client.connect().then(() => client.query('SELECT u.id, u."emailVerified", u."otpCode", c.pseudo FROM "user" u LEFT JOIN citizen c ON c."userId" = u.id ORDER BY u."createdAt" DESC LIMIT 5')).then(res => { 
-  console.table(res.rows); 
-  client.end(); 
-}).catch(err => { 
-  console.error(err); 
-  client.end(); 
-});
